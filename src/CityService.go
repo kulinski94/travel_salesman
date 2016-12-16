0d932666e636a8a80c2e6026dd6ae7f474ff0724
kulinski <tsvetelin_kulinski@abv.bg>
2016-12-16 18:32:50 +0200

    generate cities

--- src/CityService.go
+++ src/CityService.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	"math/rand"
 	"net/http"
 )
 
 func getAllCities(w http.ResponseWriter, r *http.Request) {
-	var cities Cities
-
-	cities = append(cities, City{XCord: 140, YCord: 50})
-	cities = append(cities, City{XCord: 400, YCord: 356})
-	cities = append(cities, City{XCord: 300, YCord: 150})
-	cities = append(cities, City{XCord: 150, YCord: 400})
-
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(cities); err != nil {
+	if err := json.NewEncoder(w).Encode(generateCities()); err != nil {
 		panic(err)
 	}
+}
+
+func generateCities() Cities {
+	var cities Cities
+	weight := 540
+	height := 480
+	i := 0
+	for i < 50 {
+		cities = append(cities, City{XCord: rand.Intn(weight), YCord: rand.Intn(height)})
+		i++
+	}
+	return cities
 }
